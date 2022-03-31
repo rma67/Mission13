a983dedcccd67400ff186850c689dcd38da70bff
rma67 <90296270+rma67@users.noreply.github.com>
2022-03-30 22:29:01 -0600

    Add project files.

---
+++ MySQLFUn/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySQLFUn.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFUn.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBowlersRepository _repo { get; set; }
+
+        //Constructor
+        public HomeController(IBowlersRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _repo.Bowlers
+                .ToList();
+
+            return View(blah);
+        }
+    }
+}
---
+++ MySQLFUn/Models/EFBowlersRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFUn.Models
+{
+    public class EFBowlersRepository : IBowlersRepository
+    {
+        private BowlingDbContext _context { get; set; }
+
+        public EFBowlersRepository (BowlingDbContext temp)
+        {
+            _context = temp
+        }
+
+        public IQueryable<Bowler> Bowlers => _context.Bowlers;
+    }
+}
